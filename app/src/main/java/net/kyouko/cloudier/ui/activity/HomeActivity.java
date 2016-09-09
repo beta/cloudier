09db7e2b85d674578a506335024448578bb17038
Beta Kuang <beta.kuang@gmail.com>
2016-09-09 20:14:06 +0800

    add basic layout for home activity

---
+++ app/src/main/java/net/kyouko/cloudier/ui/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package net.kyouko.cloudier.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import net.kyouko.cloudier.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
