d376d75bfed41607231f40f0abecb3969ab245be
Beta Kuang <beta.kuang@gmail.com>
2016-09-19 20:45:50 +0800

    add about for settings

--- app/src/main/java/net/kyouko/cloudier/ui/fragment/SettingsFragment.java
+++ app/src/main/java/net/kyouko/cloudier/ui/fragment/SettingsFragment.java
@@ -2,11 +2,14 @@ package net.kyouko.cloudier.ui.fragment;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 
 import net.kyouko.cloudier.R;
 import net.kyouko.cloudier.util.ImageUtil;
+
+import de.psdev.licensesdialog.LicensesDialog;
 
 /**
  * Fragment for app's settings.
@@ -21,6 +24,18 @@ public class SettingsFragment extends PreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference openSourceLicensesPref = findPreference("about.licenses");
+        openSourceLicensesPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                new LicensesDialog.Builder(getActivity())
+                        .setNotices(R.raw.notices)
+                        .build()
+                        .show();
+                return true;
+            }
+        });
     }
 
 
